727b1c3b5d1376dcf1cf5037938f981f39fceb2d
BaskarMitrah <mit44186@adobe.com>
2024-11-14 19:06:48 +0530

    Update : Remove the unwanted import

--- hlx_statics/blocks/media/media.js
+++ hlx_statics/blocks/media/media.js
@@ -1,9 +1,9 @@
-import { loadEmbed } from "../embed/embed";
 
 export default function decorate(block) {
+  console.log('block', block)
   const wrapper = block.querySelector('.media > div > div');
   const youtubeUrl = wrapper?.textContent.trim();
-
+  console.log('youtubeUrl', youtubeUrl)
   if (youtubeUrl) {
     const match = youtubeUrl.match(/https:\/\/youtu\.be\/([a-zA-Z0-9_-]+)/);
     if (match) {
@@ -11,6 +11,5 @@ export default function decorate(block) {
         <div class="youtube-mdx-embed" style="width: 100%; position: relative; padding-top: 56.25%;"><iframe data-testid="youtube" title="youTube-AwL0QRxH9JQ" src="https://www.youtube.com/embed/${match[1]}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;" spellcheck="false"></iframe></div>
         `;
     }
-    loadEmbed(block , youtubeUrl)
   }
 }
