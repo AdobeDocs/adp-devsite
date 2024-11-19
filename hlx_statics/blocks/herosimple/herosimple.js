1751bcda0bd41f4e9045f78e9992bc97a79ba9dd
manimitrah <mit80447@adobe.com>
2024-11-19 13:44:46 +0530

    fix : herosimple background image

--- hlx_statics/blocks/herosimple/herosimple.js
+++ hlx_statics/blocks/herosimple/herosimple.js
@@ -3,8 +3,8 @@ import {
   } from '../../scripts/lib-adobeio.js';
   
   /**
-   * decorates the hero
-   * @param {Element} block The hero block element
+   * decorates the herosimple
+   * @param {Element} block The herosimple block element
    */
   export default async function decorate(block) {
   
@@ -46,4 +46,46 @@ import {
         p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
       }
     });
+  const sourceElement = block.querySelector('source[type="image/webp"]');
+  const srcsetValue = sourceElement ? sourceElement.getAttribute('srcset') : null;
+  const url = srcsetValue.split(' ')[0];
+  const imgElement = block.querySelector('img');
+  const heroFirstDiv = block.querySelector('.herosimple > div');
+  const heroSecondDiv = block.querySelector('.herosimple > div:nth-of-type(2)');
+  if (srcsetValue) {
+    imgElement.style.display = 'none';
+    heroFirstDiv.style.marginBottom= '0px';
+    heroSecondDiv.style.marginTop = '0px';
+    Object.assign(block.style, {
+      backgroundImage: `url(${url})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
+    });
+  }
+
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section) => {
+    if (section.classList.contains('herosimple-container')) {
+      section.style.margin = '0px';
+      section.style.maxWidth = 'none';
+      const subParent = document.createElement('div');
+      subParent.classList.add('sub-parent');
+      const children = Array.from(section.children);
+      children.forEach(child => {
+        if (!child.classList.contains('herosimple-wrapper')) {
+          subParent.appendChild(child);
+        }
+      });
+      const herosimpleWrapper = section.querySelector('.herosimple-wrapper');
+      if (herosimpleWrapper) {
+        section.insertBefore(subParent, herosimpleWrapper.nextSibling);
+      } else {
+        section.appendChild(subParent);
+      }
+      subParent.style.margin = '0 164px';
+      subParent.style.maxWidth = '1280px';
+    }
+  });
+
   }
