cc0084122d59a82511afdf65653a69c56701d060
BaskarMitrah <mit44186@adobe.com>
2024-12-21 12:15:37 +0530

    fix : design issues

--- hlx_statics/blocks/buttonBlock/buttonBlock.js
+++ hlx_statics/blocks/buttonBlock/buttonBlock.js
@@ -5,25 +5,27 @@ import { decorateButtons } from "../../scripts/lib-adobeio.js";
  * @param {Element} block The calendar block element.
  */
 export default async function decorate(block) {
-    const { href = "#", variant = "outline", position = "start" } = block.dataset;
+    const href = block.getAttribute('data-href');
+    const variant = block.getAttribute('data-variant');
+    const position = block.getAttribute('data-position');
     const innerText = block.querySelector('div div').innerText;
 
-    const paragraphTag = document.createElement('p');
-    paragraphTag.style.cssText = `display: flex;${position ? `justify-content: ${position};` : ''}`;
+    const pTag = document.createElement('p');
+    pTag.style.cssText = `display: flex;${position ? `justify-content: ${position};` : ''}`;
 
     const anchorTag = document.createElement('a');
-    anchorTag.setAttribute('href', href);
+    anchorTag.setAttribute('href', href || '#');
     anchorTag.innerHTML = innerText;
 
     if (variant === 'primary') {
         const strongTag = document.createElement('strong');
         strongTag.appendChild(anchorTag);
-        paragraphTag.appendChild(strongTag);
+        pTag.appendChild(strongTag);
     } else {
-        paragraphTag.appendChild(anchorTag);
+        pTag.appendChild(anchorTag);
     }
 
     block.innerHTML = '';
-    block.appendChild(paragraphTag);
+    block.appendChild(pTag);
     decorateButtons(block);
 }
