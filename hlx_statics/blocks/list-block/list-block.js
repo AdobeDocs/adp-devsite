e7d676e3b3cb688d00530ce972c5914a73b0c0f9
BaskarMitrah <mit44186@adobe.com>
2025-01-07 15:55:27 +0530

    update : code format

--- hlx_statics/blocks/list-block/list-block.js
+++ hlx_statics/blocks/list-block/list-block.js
@@ -1,42 +1,29 @@
-export default function decorate(block) {
+/**
+ * decorates the list-block
+ * @param {Element} block The list-block element
+ */
+
+ export default function decorate(block) {
+  const dynamicColor = block.getAttribute('data-iconcolor');
   const listFirstDivs = document.createElement('ul');
   const listSecondDivs = document.createElement('ul');
 
-  listFirstDivs.classList.add('spectrum-Body', 'spectrum-Body--sizeM', 'custom-marker');
-  listSecondDivs.classList.add('spectrum-Body', 'spectrum-Body--sizeM', 'custom-marker');
-  
-  const dynamicColor =  block.getAttribute('data-iconcolor'); 
+  [listFirstDivs, listSecondDivs].forEach(list => {
+    list.classList.add('spectrum-Body', 'spectrum-Body--sizeM', 'custom-marker');
+    list.style.setProperty('--marker-color', dynamicColor);
+  });
 
-  listFirstDivs.style.setProperty('--marker-color', dynamicColor);
-  listSecondDivs.style.setProperty('--marker-color', dynamicColor);
-
-  const divs = block.querySelectorAll('div');
-
-  divs.forEach(parentDiv => {
-    const firstDiv = parentDiv.querySelector('div:first-child');
-    const secondDiv = parentDiv.querySelector('div:nth-child(2)');
-
-    const firstDivListItem = document.createElement('li');
-    const secondDivListItem = document.createElement('li');
-
-    if (firstDiv && firstDiv.textContent.trim() !== '') {
-      firstDivListItem.appendChild(firstDiv);
-    }
-
-    if (secondDiv && secondDiv.textContent.trim() !== '') {
-      secondDivListItem.appendChild(secondDiv);
-    }
-
-    if (firstDivListItem.hasChildNodes()) {
-      listFirstDivs.appendChild(firstDivListItem);
-    }
-
-    if (secondDivListItem.hasChildNodes()) {
-      listSecondDivs.appendChild(secondDivListItem);
-    }
+  block.querySelectorAll('div').forEach(parentDiv => {
+    [...parentDiv.children].forEach((child, index) => {
+      if (child.textContent.trim()) {
+        const listItem = document.createElement('li');
+        listItem.appendChild(child);
+        (index === 0 ? listFirstDivs : listSecondDivs).appendChild(listItem);
+      }
+    });
   });
 
   block.innerHTML = '';
-  block.appendChild(listFirstDivs);
-  block.appendChild(listSecondDivs);
+  block.append(listFirstDivs, listSecondDivs);
 }
+
