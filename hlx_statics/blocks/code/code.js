f563632bae392de87fcc42ec7f4a76203d954fa0
manimitrah <mit80447@adobe.com>
2025-01-07 19:11:26 +0530

    fix: herosimple, details and code block

--- hlx_statics/blocks/code/code.js
+++ hlx_statics/blocks/code/code.js
@@ -3,6 +3,9 @@ import decoratePreformattedCode from '../../components/code.js';
 export default function decorate(block) {
   const pre = document.createElement('pre');
   const code = block.querySelector('code');
+  if (!code.className.match(/language-/)) {
+    code.classList.add('language-bash');
+  }
   code.parentElement.replaceChild(pre, code);
   pre.appendChild(code);
   decoratePreformattedCode(block);
