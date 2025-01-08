ee5371e20d6ff51269853bbb7fc1c8f407b7f71e
manimitrah <mit80447@adobe.com>
2025-01-08 11:20:18 +0530

    fix: set the default language to none for the code block

--- hlx_statics/blocks/code/code.js
+++ hlx_statics/blocks/code/code.js
@@ -4,7 +4,7 @@ export default function decorate(block) {
   const pre = document.createElement('pre');
   const code = block.querySelector('code');
   if (!code.className.match(/language-/)) {
-    code.classList.add('language-bash');
+    code.classList.add('language-none');
   }
   code.parentElement.replaceChild(pre, code);
   pre.appendChild(code);
