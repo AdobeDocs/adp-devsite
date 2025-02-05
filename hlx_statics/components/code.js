0f28528ec9ae0b67d57a0be0c95376df127567f2
Melissa Garcia <ens15038@adobe.com>
2025-02-04 17:53:37 -0800

    refactor: move setting of language-none to util function

--- hlx_statics/components/code.js
+++ hlx_statics/components/code.js
@@ -4,6 +4,9 @@ export default function decoratePreformattedCode(block) {
   pre.classList.add('line-numbers');
 
   const code = block.querySelector('code');
+  if (!code.className.match(/language-/)) {
+    code.classList.add('language-none');
+  }
   code.setAttribute('data-prismjs-copy', 'Copy');
   code.setAttribute('data-prismjs-copy-success', 'Copied to your clipboard');
   code.setAttribute('data-prismjs-copy-timeout', '3000');
