6a3751211cd9f6cd426aedb89b37e5fbef159ccf
Melissa Garcia <ens15038@adobe.com>
2025-04-10 08:18:12 -0700

    cleanup

--- hlx_statics/components/wrapperContainer.js
+++ hlx_statics/components/wrapperContainer.js
@@ -2,23 +2,12 @@ import { createTag } from '../../hlx_statics/scripts/lib-adobeio.js'
 
 export default function insertWrapperContainer(block) {
     const wrapper = block.parentElement;
-    const container = wrapper.parentElement;
+    
     const name = block.getAttribute('data-block-name');
     const wrapperContainer = createTag('div', { class: `${name}-wrapper-container` });
     
-    
+    wrapper.replaceWith(wrapperContainer);
+    wrapperContainer.appendChild(wrapper);
 
-    
-    
-    console.log('~~ hello', wrapper, container, block, name);
+    console.log('~~ hello', wrapperContainer.parentElement);
 }
-
-
-
-// const wrapperContainer = createTag('div', { class: 'contributors-wrapper-container' });
-// const container = block.parentElement;
-// container.remove(block);
-
-// var new_parent = get_parent().get_parent()
-// get_parent().remove_child(self)
-// new_parent.add_child(self)
