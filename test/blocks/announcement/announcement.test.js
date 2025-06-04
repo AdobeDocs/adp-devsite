ef01c0d956caac34ebae03dd40dbcea7d76ab457
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2025-06-04 15:48:49 -0700

    fixed html for test with main tag out of block only

--- test/blocks/announcement/announcement.test.js
+++ test/blocks/announcement/announcement.test.js
@@ -9,38 +9,35 @@ const { loadBlock } = await import('../../../hlx_statics/scripts/lib-helix.js');
 
 const announcementBlock = document.querySelector('div.announcement');
 await loadBlock(announcementBlock);
-console.log(announcementBlock);
 
 describe('Announcement block', () => {
   it('Builds announcement block', async () => {
-    await import('../../../hlx_statics/scripts/scripts.js');
-    
     expect(announcementBlock).to.exist;
 
     // headings
     const headings = announcementBlock.querySelectorAll('h1, h2, h3, h4, h5, h6');
     expect(headings).to.exist;
     headings.forEach((heading) => {
-    //   expect(heading.classList.contains('spectrum-Heading')).to.be.true;
-    //   expect(heading.classList.contains('spectrum-Heading--sizeL')).to.be.true;
-    //   expect(heading.classList.contains('announcement-heading')).to.be.true;
-    //   expect(heading.style.whiteSpace).to.equal('normal');
+      expect(heading.classList.contains('spectrum-Heading')).to.be.true;
+      expect(heading.classList.contains('spectrum-Heading--sizeL')).to.be.true;
+      expect(heading.classList.contains('announcement-heading')).to.be.true;
+      expect(heading.style.whiteSpace).to.equal('normal');
     });
 
     // paragraphs
     const paragraphs = announcementBlock.querySelectorAll('p');
     expect(paragraphs).to.exist;
     paragraphs.forEach((paragraph) => {
-      // expect(paragraph.classList.contains('spectrum-Body')).to.be.true;
-      // expect(paragraph.classList.contains('spectrum-Body--sizeL')).to.be.true;
-      // expect(paragraph.style.whiteSpace).to.equal('normal');
+      expect(paragraph.classList.contains('spectrum-Body')).to.be.true;
+      expect(paragraph.classList.contains('spectrum-Body--sizeL')).to.be.true;
+      expect(paragraph.style.whiteSpace).to.equal('normal');
     });
 
     //picture img
     const image = announcementBlock.querySelector('picture img');
     if(!image){
       if (!announcementBlock.classList.contains('background-color-white') && !announcementBlock.classList.contains('background-color-navy') && !announcementBlock.classList.contains('background-color-dark-gray')) {
-        // expect(announcementBlock.classList.contains('background-color-gray')).to.be.true;
+        expect(announcementBlock.classList.contains('background-color-gray')).to.be.true;
       }
     }
 
