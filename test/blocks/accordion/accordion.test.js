698c87dfda16ecd375f07a9523e4f46a4dad1c30
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2025-06-04 15:27:44 -0700

    accordion test done

--- test/blocks/accordion/accordion.test.js
+++ test/blocks/accordion/accordion.test.js
@@ -13,27 +13,29 @@ await loadBlock(accordionBlock);
 
 describe('Accordion block', () => {
   it('Builds accordion block', async () => {
-    // await import('../../../hlx_statics/scripts/scripts.js');
-    
     expect(accordionBlock).to.exist;
     expect(accordionBlock.classList.contains('block')).to.be.true;
     expect(accordionBlock.classList.contains('accordion')).to.be.true;
     expect(accordionBlock.getAttribute('daa-lh')).to.equal('accordion');
+  });
 
-    //general title
+  it('accordion > general title', async () => {
     const title = accordionBlock.querySelector('h1, h2');
     expect(title).to.exist;
+  });
 
-    //accordion title
+  it('accordion > accordion title', async () => {
     const accordion_title = accordionBlock.querySelector('.accordion-title');
     expect(accordion_title.querySelector('h1')).to.exist;
+  });
 
-    //accordion div
+  it('accordion > accordion div', async () => {
     const accordion_div = accordionBlock.querySelector('.accordion-div');
     expect(accordion_div).to.exist;
+  });
 
-    //accordion items
-    const accordion_items = accordion_div.querySelectorAll('.accordion-item');
+  it('accordion > accordion items', async () => {
+    const accordion_items = accordionBlock.querySelectorAll('.accordion-item');
     accordion_items.forEach((item) => {
       const heading = item.querySelector('h3, h4, h5, h6');
       expect(heading).to.exist;
@@ -51,21 +53,23 @@ describe('Accordion block', () => {
       const p = accordion_item_content.querySelector('p');
       expect(p).to.exist;
 
+      //before click
+      expect(accordion_item_header .classList.contains('active')).to.be.false;
+      
       //heading click
       item.click();
-      // expect(heading.classList.contains('active')).to.be.true;
+      expect(accordion_item_header .classList.contains('active')).to.be.true;
       expect(accordion_item_content.style.display).to.equal('block');
       expect(accordion_item_header.querySelector('.spectrum-UIIcon-ChevronRight100').style.display).to.equal('none');
       expect(accordion_item_header.querySelector('.spectrum-UIIcon-ChevronDown100').style.display).to.equal('block');
 
       //heading click again
       item.click();
-      expect(heading.classList.contains('active')).to.be.false;
+      expect(accordion_item_header .classList.contains('active')).to.be.false;
       expect(accordion_item_content.style.display).to.equal('none');
       expect(accordion_item_header.querySelector('.spectrum-UIIcon-ChevronRight100').style.display).to.equal('block');
       expect(accordion_item_header.querySelector('.spectrum-UIIcon-ChevronDown100').style.display).to.equal('none');
 
     });
   });
-
 });
