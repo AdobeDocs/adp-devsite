ca1b57135bc107d03bb1a27d6ca639eda1911ec7
BaskarMitrah <mit44186@adobe.com>
2025-06-09 11:59:07 +0530

    fix : announcement - button issue

--- hlx_statics/blocks/announcement/announcement.js
+++ hlx_statics/blocks/announcement/announcement.js
@@ -72,10 +72,11 @@ export default async function decorate(block) {
     p.style.whiteSpace = "normal";
   });
   const imageExists = block.querySelector('picture img');
+  const allowed = ["background-color-white", "background-color-navy", "background-color-dark-gray"];
   if (!imageExists) {
     if (getMetadata("template") === "documentation" && background_Exists) {
-        block.classList.add(background_Exists);
-    } else if (!block.classList.matches(".background-color-white, .background-color-navy, .background-color-dark-gray")) {
+      block.classList.add(background_Exists);
+    } else if (!allowed.some(cls => block.classList.contains(cls))) {
       block.classList.add("background-color-gray");
     }
   }
