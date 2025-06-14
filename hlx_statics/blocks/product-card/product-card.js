a5f86278cdab3d0f7cec4336cf788e052ea67282
BaskarMitrah <mit44186@adobe.com>
2025-06-14 16:16:11 +0530

    fix : product card - button issue

--- hlx_statics/blocks/product-card/product-card.js
+++ hlx_statics/blocks/product-card/product-card.js
@@ -1,4 +1,4 @@
-import { createTag } from '../../scripts/lib-adobeio.js';
+import { createTag, decorateButtons } from '../../scripts/lib-adobeio.js';
 import { getMetadata } from '../../scripts/scripts.js';
 
 /**
@@ -6,19 +6,22 @@ import { getMetadata } from '../../scripts/scripts.js';
  * @param {Element} block The title block element {Parameter Type} Name of the Parameter
  */
 export default async function decorate(block) {
+    console.log("block--", block)
     block.setAttribute('daa-lh', 'product-card');
     block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
         h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeS', 'title-heading');
     });
     block.querySelectorAll('p, div').forEach(p => {
         if (p.textContent.trim() || p.tagName === 'P') {
+            console.log("p :) :)", p)
             p.classList.add('spectrum-Body', 'spectrum-Body--sizeM');
         }
     });
+
     const width = block?.parentElement?.parentElement?.getAttribute('data-width');
     Array.from(block.children).forEach(div => {
         div.style.width = width;
-    });
+    })
 
     if (getMetadata('template') === 'documentation') {
         Array.from(block.children).forEach((card) => {
@@ -53,7 +56,9 @@ export default async function decorate(block) {
         });
     } else {
         Array.from(block.children).forEach((div) => {
-            const newDiv = createTag('div', { class: 'all-button-container' })
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('all-button-container')
+
             div.lastElementChild.querySelectorAll('.button-container').forEach((p) => {
                 newDiv.append(p);
             })
@@ -66,8 +71,10 @@ export default async function decorate(block) {
         const anchors = card.querySelectorAll('a');
         anchors.forEach((a, index) => {
             const isWrappedInStrong = a.parentElement.tagName === 'STRONG';
-            if (isWrappedInStrong || (getMetadata('template') === 'documentation') && index === 1) {
+            if ((index === 0 || index === 1) && isWrappedInStrong || index === 1) {
                 a.className = "spectrum-Button spectrum-Button--outline spectrum-Button--accent spectrum-Button--sizeM";
+            } else if (!isWrappedInStrong) {
+                a.className = "spectrum-Button spectrum-Button--outline spectrum-Button--secondary spectrum-Button--sizeM";
             } else {
                 a.className = "spectrum-Button spectrum-Button--sizeM spectrum-Button--outline spectrum-Button--secondary";
             }
