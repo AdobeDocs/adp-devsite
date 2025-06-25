e960ce038a398ea01f957cb5349297108d3f684b
BaskarMitrah <mit44186@adobe.com>
2025-06-25 12:09:18 +0530

    feat : black banner

--- hlx_statics/blocks/banner/banner.js
+++ hlx_statics/blocks/banner/banner.js
@@ -1,10 +1,35 @@
+import { decorateButtons, removeEmptyPTags } from '../../scripts/lib-adobeio.js';
+
 /**
- * decorates the banenr
- * @param {Element} block The banner block element
+ * Decorates the banner block
+ * @param {Element} block - The banner block element
  */
 export default async function decorate(block) {
-  block.setAttribute('daa-lh', 'banner');
-  block.querySelectorAll('h1').forEach((h1) => {
-    h1.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXL');
+  block.classList.forEach(clr => {
+    if (clr.startsWith('black-banner')) {
+      block.parentElement.classList.add(clr);
+    }
   });
+
+  block.setAttribute('daa-lh', 'banner');
+
+  const h1s = block.querySelectorAll('h1');
+  const contentEls = block.querySelectorAll('p, div');
+
+  if (h1s.length > 0) {
+    block.classList.add('isH');
+    h1s.forEach(h1 => h1.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXL'));
+  }
+
+  contentEls.forEach(el => {
+    const hasText = el.textContent.trim().length > 0;
+
+    if (el.tagName === 'P') block.classList.add('isP');
+    if (hasText || el.tagName === 'P') {
+      el.classList.add('spectrum-Body', 'spectrum-Body--sizeM');
+    }
+  });
+
+  decorateButtons(block);
+  removeEmptyPTags(block);
 }
