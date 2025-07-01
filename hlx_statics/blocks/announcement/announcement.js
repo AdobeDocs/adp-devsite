db276ec8e66a656d9e040a9ed48e4cd6eb283d8c
BaskarMitrah <mit44186@adobe.com>
2025-07-01 13:27:08 +0530

    Fix : announcement-block border design issue

--- hlx_statics/blocks/announcement/announcement.js
+++ hlx_statics/blocks/announcement/announcement.js
@@ -63,6 +63,7 @@ function setBackgroundImage(block) {
  */
 export default async function decorate(block) {
   const backgroundColor = block.getAttribute("data-backgroundcolor");
+  const isBorder =  block.getAttribute("data-isborder");
   block.setAttribute('daa-lh', 'announcement');
   block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
     h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeL', 'announcement-heading');
@@ -77,6 +78,9 @@ export default async function decorate(block) {
 
   if (!imageExists) {
     if (getMetadata("template") === "documentation" && allowedBackgroundColors.includes(backgroundColor)) {
+      if (isBorder === "true") {
+        block.classList.add("isborder");
+      }
       block.className = block.className.split(/\s+/).filter(c => !c.startsWith('background-color-')).join(' ').trim();
       block.classList.add(backgroundColor);
     }
