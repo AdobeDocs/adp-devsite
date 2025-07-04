25de7e02d6ee9834ff35604c33a130f3d27cdde5
Melissa Garcia <ens15038@adobe.com>
2025-07-04 13:09:33 -0700

    feat: rename hero default variant to halfwidth

--- hlx_statics/blocks/hero/hero.js
+++ hlx_statics/blocks/hero/hero.js
@@ -22,53 +22,54 @@ function rearrangeLinks(block) {
  * @param {Element} block The hero block element
  */
 export default async function decorate(block) {
-  block.setAttribute('daa-lh', 'hero');
-  // Block decoration
-  decorateLightOrDark(block, true);
-  // H1 decoration
-  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
-    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL');
-  });
+  if(block.classList.contains('halfwidth')) {
+    block.setAttribute('daa-lh', 'hero');
+    // Block decoration
+    decorateLightOrDark(block, true);
+    // H1 decoration
+    block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+      h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL');
+    });
 
-  block.querySelectorAll('picture source').forEach((picture) => {
-    // Removes weird max-width attribute
-    picture.media = '';
-  });
+    block.querySelectorAll('picture source').forEach((picture) => {
+      // Removes weird max-width attribute
+      picture.media = '';
+    });
 
 
-  // Removes content for span.icon
-  block.querySelectorAll('span.icon').forEach((span) => {
-    span.textContent = '';
-  });
-  // Link decoration
-  rearrangeLinks(block);
-  decorateButtons(block);
-  // Paragraph decoration
-  block.querySelectorAll('p').forEach((p) => {
-    if (p.innerText) {
-      p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+    // Removes content for span.icon
+    block.querySelectorAll('span.icon').forEach((span) => {
+      span.textContent = '';
+    });
+    // Link decoration
+    rearrangeLinks(block);
+    decorateButtons(block);
+    // Paragraph decoration
+    block.querySelectorAll('p').forEach((p) => {
+      if (p.innerText) {
+        p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+      }
+    });
+
+    const backgroundImage = block.classList.contains('full-width-background');
+    const heroWrapper = block?.parentElement?.parentElement;
+
+    if (backgroundImage) {
+      const picsrc = block.querySelectorAll('picture img')[0].currentSrc;
+      heroWrapper.querySelectorAll('.hero-container > div').forEach((herowrapper) => {
+        Object.assign(herowrapper.style, {
+          backgroundImage: `url(${picsrc})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        });
+      });
+      heroWrapper.querySelectorAll('.hero-container > div > div').forEach((herowrapper) => {
+        Object.assign(herowrapper.style, {
+          backgroundColor: 'transparent',
+          width: '75%',
+          margin: 'auto',
+        });
+      });
     }
-  });
-
-  const backgroundImage = block.classList.contains('full-width-background');
-  const heroWrapper = block?.parentElement?.parentElement;
-
-  if (backgroundImage) {
-    const picsrc = block.querySelectorAll('picture img')[0].currentSrc;
-    heroWrapper.querySelectorAll('.hero-container > div').forEach((herowrapper) => {
-      Object.assign(herowrapper.style, {
-        backgroundImage: `url(${picsrc})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-      });
-    });
-    heroWrapper.querySelectorAll('.hero-container > div > div').forEach((herowrapper) => {
-      Object.assign(herowrapper.style, {
-        backgroundColor: 'transparent',
-        width: '75%',
-        margin: 'auto',
-      });
-    });
   }
-
 }
