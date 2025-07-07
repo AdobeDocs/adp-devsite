a03c84537ab72c0c45792b300453572a0978f0b5
Melissa Garcia <ens15038@adobe.com>
2025-07-07 14:30:04 -0700

    feat: copy and paste site-hero into hero

--- hlx_statics/blocks/hero/hero.js
+++ hlx_statics/blocks/hero/hero.js
@@ -1,5 +1,7 @@
 import {
   decorateButtons,
+  removeEmptyPTags,
+  createTag,
 } from '../../scripts/lib-adobeio.js';
 import { decorateLightOrDark } from '../../scripts/lib-helix.js';
 
@@ -71,5 +73,31 @@ export default async function decorate(block) {
         });
       });
     }
+  } else if(block.classList.contains('site-hero')) {
+    block.setAttribute('daa-lh', 'site hero');
+    removeEmptyPTags(block);
+    decorateButtons(block);
+  
+    const button_div = createTag('div', {class: 'hero-button-container'});
+  
+    block.classList.add('spectrum--dark');
+    block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+      h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL');
+      h.style.color = "white";
+      h.parentElement.classList.add('hero-content');
+      h.parentElement.append(button_div);
+    });
+  
+    block.querySelectorAll('p').forEach((p) => {
+      const hasLinks = p.querySelectorAll('a, button');
+      // don't attach to icon container or if p tag contains links
+      if (!p.classList.contains('icon-container')) {
+        p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+        p.style.color = "white";
+      }
+      if (p.classList.contains('button-container')){
+        button_div.append(p);
+      }
+    });
   }
 }
