545cf6661965251bc5b089e2178e89bc80ec5ce3
Tim Kim <timk@adobe.com>
2025-08-25 16:17:52 -0700

    Fix config loading locally in local dev mode (#296)
    * fix: pull locally from config when in local dev mode
    
    * fix: Remove unnecessary piece

--- hlx_statics/blocks/fragment/fragment.js
+++ hlx_statics/blocks/fragment/fragment.js
@@ -19,49 +19,55 @@ import {
    */
   export async function loadFragment(path) {
     if (path) {
-      const fetchPathUrl = `${path}.plain.html`;
+      // special case for config to load locally as it won't have the .plain.html path
+
+      const lastSlashIndex = path?.lastIndexOf('/');
+      const lastPrefix = path?.substring(lastSlashIndex + 1);
+
+      const fetchPathUrl = (lastPrefix && lastPrefix === 'config') ? `${path}` : `${path}.plain.html`;
 
       const hashCode = (s) => s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0);
       const fragmentHash = `${hashCode(fetchPathUrl)}`;
       let main;
 
-      if (sessionStorage.getItem(fragmentHash)) {
+    if (sessionStorage.getItem(fragmentHash)) {
+      main = document.createElement('main');
+      main.innerHTML = sessionStorage.getItem(fragmentHash);
+      return main;
+    } else {
+      const resp = await fetch(fetchPathUrl);
+      if (resp.ok) {
         main = document.createElement('main');
-        main.innerHTML = sessionStorage.getItem(fragmentHash);
-        return main;
-      } else {
-        const resp = await fetch(`${path}.plain.html`);
-        if (resp.ok) {
-          main = document.createElement('main');
-          main.innerHTML = await resp.text();
-          sessionStorage.setItem(fragmentHash, main.innerHTML);
-          // reset base path for media to fragment base
-          const resetAttributeBase = (tag, attr) => {
-            main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-              elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-            });
-          };
-          resetAttributeBase('img', 'src');
-          resetAttributeBase('source', 'srcset');
+        main.innerHTML = await resp.text();
 
-          decorateMain(main);
-          await loadSections(main);
-          return main;
-        }
-      }
-    }
-    return null;
-  }
-  
-  export default async function decorate(block) {
-    const link = block.querySelector('a');
-    const path = link ? link.getAttribute('href') : block.textContent.trim();
-    const fragment = await loadFragment(path);
-    if (fragment) {
-      const fragmentSection = fragment.querySelector(':scope .section');
-      if (fragmentSection) {
-        block.closest('.section').classList.add(...fragmentSection.classList);
-        block.closest('.fragment').replaceWith(...fragment.childNodes);
+        sessionStorage.setItem(fragmentHash, main.innerHTML);
+        // reset base path for media to fragment base
+        const resetAttributeBase = (tag, attr) => {
+          main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+            elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+          });
+        };
+        resetAttributeBase('img', 'src');
+        resetAttributeBase('source', 'srcset');
+
+        decorateMain(main);
+        await loadSections(main);
+        return main;
       }
     }
   }
+  return null;
+}
+
+export default async function decorate(block) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      block.closest('.section').classList.add(...fragmentSection.classList);
+      block.closest('.fragment').replaceWith(...fragment.childNodes);
+    }
+  }
+}
