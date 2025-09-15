6ab6bb7eee2dcf407e30c3cc0355199395696132
Melissa Garcia <ens15038@adobe.com>
2025-09-15 10:31:50 -0700

    refactor: extract for readability

--- hlx_statics/blocks/superhero/superhero.js
+++ hlx_statics/blocks/superhero/superhero.js
@@ -12,29 +12,33 @@ export default async function decorate(block) {
   const isDevDocs = main.classList.contains('dev-docs');
 
   if (isDevBiz && ['centered', 'centeredxl'].some((variant) => block.classList.contains(variant))) {
-    removeEmptyPTags(block);
-    decorateButtons(block);
-
-    const button_div = createTag('div', { class: 'hero-button-container' });
-
-    block.classList.add('spectrum--dark');
-    block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
-      h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL');
-      h.style.color = 'white';
-      h.parentElement.classList.add('superhero-content');
-      h.parentElement.append(button_div);
-    });
-
-    block.querySelectorAll('p').forEach((p) => {
-      const hasLinks = p.querySelectorAll('a, button');
-      // don't attach to icon container or if p tag contains links
-      if (!p.classList.contains('icon-container')) {
-        p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
-        p.style.color = 'white';
-      }
-      if (p.classList.contains('button-container')) {
-        button_div.append(p);
-      }
-    });
+    decorateDevBizCentered(block);
   }
+}
+
+async function decorateDevBizCentered(block) {
+  removeEmptyPTags(block);
+  decorateButtons(block);
+
+  const button_div = createTag('div', { class: 'hero-button-container' });
+
+  block.classList.add('spectrum--dark');
+  block.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((h) => {
+    h.classList.add('spectrum-Heading', 'spectrum-Heading--sizeXXL');
+    h.style.color = 'white';
+    h.parentElement.classList.add('superhero-content');
+    h.parentElement.append(button_div);
+  });
+
+  block.querySelectorAll('p').forEach((p) => {
+    const hasLinks = p.querySelectorAll('a, button');
+    // don't attach to icon container or if p tag contains links
+    if (!p.classList.contains('icon-container')) {
+      p.classList.add('spectrum-Body', 'spectrum-Body--sizeL');
+      p.style.color = 'white';
+    }
+    if (p.classList.contains('button-container')) {
+      button_div.append(p);
+    }
+  });
 }
