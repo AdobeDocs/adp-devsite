ca14ffc4f8b48f07c8328f124dff8d17ffab56c6
Melissa Garcia <ens15038@adobe.com>
2025-09-17 11:51:04 -0700

    fix: remove because hero is a devbiz only block

--- hlx_statics/scripts/scripts.js
+++ hlx_statics/scripts/scripts.js
@@ -626,7 +626,7 @@ async function loadLazy(doc) {
     footer.style.gridArea = 'footer';
     main.append(footer);
 
-    const hasHero = Boolean(document.querySelector('.hero, .herosimple'));
+    const hasHero = Boolean(document.querySelector('.herosimple'));
     const hasResources = Boolean(document.querySelector('.resources-wrapper'));
     const hasHeading = main.querySelectorAll('h2:not(.side-nav h2):not(footer h2), h3:not(.side-nav h3):not(footer h3)').length !== 0;
     if (!hasHero && hasHeading) {
