e807b21bad104dd62025e7227f3447840c48748f
Melissa Garcia <ens15038@adobe.com>
2025-12-17 08:37:36 -0800

    refactor: remove old way of parsing attrs (new way parses dash connected attrs to bypass eds limitations)

--- hlx_statics/blocks/code/code.js
+++ hlx_statics/blocks/code/code.js
@@ -1,13 +1,9 @@
-import decoratePreformattedCode, { applyLanguageDirectives, extractLanguageDirectives } from '../../components/code.js';
+import decoratePreformattedCode from '../../components/code.js';
 
 export default function decorate(block) {
-  const language = extractLanguageDirectives(block);
   const code = block.querySelector('code');
   const pre = document.createElement('pre');
   code.parentElement.replaceChild(pre, code);
   pre.appendChild(code);
-  if (language) {
-    applyLanguageDirectives(pre, code, language);
-  }
   decoratePreformattedCode(block);
 }
