fa30150d685469c71085108433c5dab9b00d7866
Louisa Chu <louisa@ensemble.com>
2025-12-23 16:56:32 -0800

    update fragment to load properly

--- hlx_statics/blocks/fragment/fragment.js
+++ hlx_statics/blocks/fragment/fragment.js
@@ -33,27 +33,29 @@ import {
     if (sessionStorage.getItem(fragmentHash)) {
       main = document.createElement('main');
       main.innerHTML = sessionStorage.getItem(fragmentHash);
-      return main;
     } else {
       const resp = await fetch(fetchPathUrl);
       if (resp.ok) {
         main = document.createElement('main');
         main.innerHTML = await resp.text();
-
         sessionStorage.setItem(fragmentHash, main.innerHTML);
-        // reset base path for media to fragment base
-        const resetAttributeBase = (tag, attr) => {
-          main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-            elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-          });
-        };
-        resetAttributeBase('img', 'src');
-        resetAttributeBase('source', 'srcset');
-
-        decorateMain(main);
-        await loadSections(main);
-        return main;
       }
+    }
+
+    // Always run initialization for both cached and fresh fragments
+    if (main) {
+      // reset base path for media to fragment base
+      const resetAttributeBase = (tag, attr) => {
+        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+        });
+      };
+      resetAttributeBase('img', 'src');
+      resetAttributeBase('source', 'srcset');
+
+      decorateMain(main);
+      await loadSections(main);
+      return main;
     }
   }
   return null;
