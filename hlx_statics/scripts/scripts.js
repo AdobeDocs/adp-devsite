bdf59823aa4e56591bae4e644e48be70ebd937a8
BaskarMitrah <mit44186@adobe.com>
2026-01-12 11:15:21 +0530

    update : express url

--- hlx_statics/scripts/scripts.js
+++ hlx_statics/scripts/scripts.js
@@ -840,9 +840,9 @@ function loadPrism(document) {
               const sessionId = pre?.getAttribute('data-playground-session-id');
               const playgroundMode = pre?.getAttribute('data-playground-mode');
               const playgroundExecutionMode = pre?.getAttribute('data-playground-execution-mode');
-              const expressURL = pre?.getAttribute('data-playground-url');
+              const expressURL = pre?.getAttribute('data-express-url');
               if (!sessionId || !playgroundMode || !playgroundExecutionMode || !expressURL) return null;
-              const btn = createTag('button' , {class : 'try-code-button'});
+              const btn = createTag('button', { class: 'try-code-button' });
               btn.textContent = 'Try in playground';
               const url = `${expressURL}?mode=${playgroundMode}&session=new&sessionId=${sessionId}&executionMode=${playgroundExecutionMode}`;
               btn.onclick = () => {
