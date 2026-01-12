766bf308404ae2dd9d811c3f6dbb563c818e2c46
yuxuanj <yuxuanj@adobe.com>
2026-01-12 08:17:05 -0800

    replaced octokit dependency with rest api call

--- test/coverage-comment.js
+++ test/coverage-comment.js
@@ -1,26 +1,28 @@
-const { Octokit } = require('@octokit/rest');
 const generateCoverageReport = require('./coverage-generate.js');
-
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-});
 
 const owner = 'AdobeDocs';
 const repo = 'adp-devsite';
 const pull_number = process.env.PR_ID;
 
 async function createComment(comment) {
-    // Github API Doc: https://docs.github.com/en/rest/pulls/comments?apiVersion=2022-11-28#list-review-comments-in-a-repository
-
-    // looks like github changed the name of the function from rest.pulls.createComment to rest.issues.createComment 
-    // and the parameter name pull_number to issue_number 
-    // :<
-    await octokit.rest.issues.createComment({
-        owner: owner,
-        repo: repo,
-        issue_number: pull_number,
-        body: comment
+    // Github API Doc: https://docs.github.com/en/rest/pulls/comments?apiVersion=2022-11-28#create-a-review-comment-for-a-pull-request
+    // note that "issue" is used to create general comments on pr
+    // and "pulls" is used to create code review/code change suggestions on pr
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues/${pull_number}/comments`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/vnd.github+json",
+            "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ body: comment })
     });
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(`Failed to post comment: ${response.status} - ${errorMessage}`);
+    }
     console.log('Coverage report comment posted successfully.');
 }
 
