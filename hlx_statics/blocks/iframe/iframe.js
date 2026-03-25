ebe775197fe46dee293c9759212ca0002210a56f
BaskarMitrah <mit44186@adobe.com>
2026-03-25 12:34:04 +0530

    fix : video blank issue

--- hlx_statics/blocks/iframe/iframe.js
+++ hlx_statics/blocks/iframe/iframe.js
@@ -183,10 +183,10 @@ export default async function decorate(block) {
   }
 
   if (width) {
-    iframe.style.width = width;
+    iframe.style.setProperty('width', width, 'important');
   }
   if (height) {
-    iframe.style.height = height;
+    iframe.style.setProperty('height', height, 'important');
   }
 
   penpalScript.onload = () => {
