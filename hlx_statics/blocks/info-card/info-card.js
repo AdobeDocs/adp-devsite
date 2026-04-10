a96a727406a710833ee6ce5fd36520a0c8e403a0
BaskarMitrah <mit44186@adobe.com>
2026-04-10 16:16:45 +0530

    fetch static data

--- hlx_statics/blocks/info-card/info-card.js
+++ hlx_statics/blocks/info-card/info-card.js
@@ -8,19 +8,39 @@ import {
  * @param {Element} block The info-card block element
  */
 export default async function decorate(block) {
-  console.log('info-card', block);
+
   block.setAttribute('daa-lh', 'info-card');
   removeEmptyPTags(block);
+
+  if (block.classList.contains('articles')) {
+    console.log('articles', block);
+
+    const path = "https://blog.developer.adobe.com/en/publish/2026/03/build-faster-with-the-adobe-express-developer-mcp-server";
+    const resp = await fetch(path);
+    if (!resp || !resp.ok) {
+      a.remove();
+      // eslint-disable-next-line no-console
+      console.log(`Could not retrieve metadata for ${path}`);
+      return;
+    }
+
+    const html = await resp.text();
+    console.log('html', html);
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    console.log('doc', doc);
+  }
+
   let containerParent;
   if (block.classList.contains('primarybutton')) {
     const primaryButton = block.querySelectorAll('a')[0];
     const up = primaryButton.parentElement;
-    const container = createTag('p', {class: 'button-container'});
+    const container = createTag('p', { class: 'button-container' });
     containerParent = primaryButton.parentElement.parentElement.parentElement;
     containerParent.appendChild(container);
     container.appendChild(up);
     if (!primaryButton.querySelector('img')) {
-      if (up.childNodes.length === 1 && up.tagName === 'STRONG'){
+      if (up.childNodes.length === 1 && up.tagName === 'STRONG') {
         primaryButton.className = 'button primary';
       }
     }
@@ -41,43 +61,43 @@ export default async function decorate(block) {
       a.appendChild(imageDiv);
     }
 
-    const textDiv = createTag('div', { class: 'cards-card-body' });   
-    
+    const textDiv = createTag('div', { class: 'cards-card-body' });
+
     const headingElement = row.querySelector('h1, h2, h3, h4, h5, h6') || row.querySelector('a');
     if (headingElement) {
       const anchorHref = row.querySelector('a');
       if (anchorHref) {
         const h3 = document.createElement('h3');
-        h3.classList.add('spectrum-Heading', 'spectrum-Heading--sizeS','card-heading');
+        h3.classList.add('spectrum-Heading', 'spectrum-Heading--sizeS', 'card-heading');
         h3.textContent = headingElement.textContent.trim();
         textDiv.appendChild(h3);
-        headingElement.href ?  a.href = headingElement.href : a.href = anchorHref.href ;
+        headingElement.href ? a.href = headingElement.href : a.href = anchorHref.href;
       } else {
-        headingElement.classList.add('spectrum-Heading', 'spectrum-Heading--sizeS','card-heading');
+        headingElement.classList.add('spectrum-Heading', 'spectrum-Heading--sizeS', 'card-heading');
         textDiv.appendChild(headingElement);
       }
     }
-          
+
     const description = row.querySelector('p') || row.querySelector('.info-card > div > div:last-child');
     if (description && description.textContent.trim() !== '') {
       const p = document.createElement('p');
       p.style.color = 'rgb(110, 110, 110)';
       p.innerHTML = description.innerHTML;
       textDiv.appendChild(p);
-    }    
-            
+    }
+
     a.appendChild(textDiv);
     li.appendChild(a);
     ul.appendChild(li);
 
   });
-  
+
   block.textContent = '';
   block.appendChild(ul);
 
   block.querySelectorAll('.icon').forEach((s) => {
     const p_parent = s.parentElement;
-    const div_parent = createTag('div', {class: 'icon-div'});
+    const div_parent = createTag('div', { class: 'icon-div' });
     p_parent.classList.add('icon-p');
     p_parent.parentElement.appendChild(div_parent);
     div_parent.appendChild(p_parent)
