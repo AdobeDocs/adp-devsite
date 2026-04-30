9034d566f56b735071a2aa98ae9d2776ae50acb8
Diane Batres <dianeba3@gmail.com>
2026-04-30 18:15:41 -0400

    test: change src of images

--- hlx_statics/blocks/superhero/superhero.js
+++ hlx_statics/blocks/superhero/superhero.js
@@ -117,7 +117,8 @@ async function decorateDevBizHalfWidth(block) {
 
   if (backgroundImage) {
     const placeholderDiv = block.querySelector('div:nth-child(2)');
-    const picSrc = placeholderDiv.querySelectorAll('picture img')[0].currentSrc;
+    const picSrc = placeholderDiv.querySelectorAll('picture img')[0].src;
+    console.log("picSrc: " + picSrc);
     Object.assign(wrapper.style, {
       backgroundImage: `url(${picSrc})`,
       backgroundRepeat: 'no-repeat',
@@ -192,7 +193,8 @@ async function decorateDevBizDefault(block) {
   });
 
   const img = block.querySelector('picture > img');
-  const url = img?.getAttribute('src');
+  const url = img.src;
+  console.log("url src: " + url);
   const pictureElement = block.querySelector('picture');
 
   const parentDiv = pictureElement?.parentElement;
