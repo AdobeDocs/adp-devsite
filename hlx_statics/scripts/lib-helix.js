cdf2ca6baa540ebb56166a7ece985ceb5d7ae464
David Sima <davids@ensemble.com>
2026-06-11 17:20:03 +0300

    fix: fixed ai assistant not showing on local

--- hlx_statics/scripts/lib-helix.js
+++ hlx_statics/scripts/lib-helix.js
@@ -11,6 +11,7 @@
  */
 
 import { loadFragment } from '../blocks/fragment/fragment.js';
+import { isLocalHostEnvironment, isStageEnvironment } from './lib-adobeio.js';
 
 /**
  * log RUM if part of the sample.
@@ -118,7 +119,7 @@ export const IS_DEV_DOCS = Boolean(getMetadata('githubblobpath'));
 if (localStorage.getItem('ai-assistant-enabled') !== 'false') {
   localStorage.setItem('ai-assistant-enabled', 'true');
 }
-export const IS_AI_ASSISTANT_ENABLED = localStorage.getItem('ai-assistant-enabled') === 'true';
+export const IS_AI_ASSISTANT_ENABLED = localStorage.getItem('ai-assistant-enabled') === 'true' && (isStageEnvironment(window.location.host, true) || isLocalHostEnvironment(window.location.host));
 window.ENABLE_AI_ASSISTANT = () => {
   localStorage.setItem('ai-assistant-enabled', 'true');
   window.location.reload();
