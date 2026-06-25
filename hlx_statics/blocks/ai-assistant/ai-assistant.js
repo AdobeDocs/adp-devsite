8716e0d4f83cce34f0cb86d90044ed26febe19cf
David Sima <davids@ensemble.com>
2026-06-26 01:45:04 +0300

    fix(ai-assistant): fixed rendered links not opening in a new tab

--- hlx_statics/blocks/ai-assistant/ai-assistant.js
+++ hlx_statics/blocks/ai-assistant/ai-assistant.js
@@ -45,7 +45,8 @@ export default async function decorate(block) {
            * @param {string} options.text
            */
           link({ href, title, text }) {
-            return `<a href="${href}" title="${title || text}" daa-ll="DevsiteAI Assistant:Message:Link:${title || text}|${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+            const analyticsLabel = `DevsiteAI Assistant:Message:Link:${title || text}|${href}`;
+            return `<a href="${href}" title="${title || text}" data-ll="${analyticsLabel}" target="_blank" rel="noopener noreferrer">${text}</a>`;
           },
         },
       });
@@ -53,6 +54,15 @@ export default async function decorate(block) {
         document.body,
         "https://unpkg.com/dompurify@^3/dist/purify.min.js",
         () => {
+          // @ts-expect-error - DOMPurify is not on the Window object
+          window.DOMPurify.setConfig({ ADD_ATTR: ["daa-ll", "daa-lh", "target"] });
+          // @ts-expect-error - DOMPurify is not on the Window object
+          window.DOMPurify.addHook('afterSanitizeAttributes', (node) => {
+            console.log('afterSanitizeAttributes')
+            if ('data-ll' in node) {
+              node.setAttribute('daa-ll', node.getAttribute('data-ll'));
+            }
+          });
           restoreChatHistory();
         },
       );
