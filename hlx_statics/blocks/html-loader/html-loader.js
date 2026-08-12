304fbfb9e6b539ae9e0b82b8adc51ac64205da73
BaskarMitrah <mit44186@adobe.com>
2026-08-12 19:50:40 +0530

    test

--- hlx_statics/blocks/html-loader/html-loader.js
+++ hlx_statics/blocks/html-loader/html-loader.js
@@ -2,26 +2,7 @@ export default async function decorate(block) {
   const code = block.querySelector('pre code');
   if (!code) return;
 
-  const html = code.textContent;
+  block.innerHTML(code)
 
-  // Parse the HTML string into a document
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
 
-  block.textContent = '';
-
-  // Move body content in
-  block.append(...doc.body.childNodes);
-
-  // Re-create scripts so they actually execute (innerHTML-inserted scripts don't run)
-  const scripts = block.querySelectorAll('script');
-  scripts.forEach((oldScript) => {
-    const newScript = document.createElement('script');
-    if (oldScript.src) {
-      newScript.src = oldScript.src;
-    } else {
-      newScript.textContent = oldScript.textContent;
-    }
-    oldScript.replaceWith(newScript);
-  });
 }
