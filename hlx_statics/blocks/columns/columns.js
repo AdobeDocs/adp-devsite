6d0385b455d4908d22f0ae16941f6b1382e8d4c8
BaskarMitrah <mit44186@adobe.com>
2026-08-28 12:13:44 +0530

    test

--- hlx_statics/blocks/columns/columns.js
+++ hlx_statics/blocks/columns/columns.js
@@ -40,9 +40,11 @@ export default async function decorate(block) {
   const variant = block.getAttribute('data-variant')
   const isReversed = block.getAttribute('data-isreversed') === 'true';
   const isControls = block.classList.contains('controls');
-  isReversed && block.classList.add('isReversed');
+  const isBackground = block.classList.contains('background');
   const isDocs = IS_DEV_DOCS;
 
+  isReversed && block.classList.add('isReversed');
+  isBackground && block.classList.add('background');
   isDocs && block.classList.add('isDocs')
   variant === "vertical" && block.classList.add(variant);
 
@@ -68,9 +70,9 @@ export default async function decorate(block) {
     } else if (hasIcons.length > 0) {
       p.classList.add('icon-container');
       // Wraps non-hyperlinked text after icon in a paragraph tag
-      p.childNodes.forEach( (child) => {
+      p.childNodes.forEach((child) => {
         if (child.nodeType === Node.TEXT_NODE) {
-          const textParagraph = createTag('p', {class:'icon-text'});
+          const textParagraph = createTag('p', { class: 'icon-text' });
           textParagraph.innerText = child.textContent;
           p.replaceChild(textParagraph, child);
         }
@@ -86,7 +88,7 @@ export default async function decorate(block) {
 
     const repeatRows = block.children;
 
-    if(slotNames.includes('video') || block.classList.contains('video')) {
+    if (slotNames.includes('video') || block.classList.contains('video')) {
       Array.from(repeatRows).forEach((repeatRow) => {
         const slotElements = Object.fromEntries(
           Array.from(repeatRow.children).map((child, index) => [slotNames[index], child])
@@ -103,9 +105,9 @@ export default async function decorate(block) {
             loop: true,
           });
           slotElements.video?.replaceWith(wrapperVideo);
-  
+
           const newWrapper = createTag('div');
-          Array.from(repeatRow.children).forEach((child) => child !== wrapperVideo && newWrapper.appendChild(child) );
+          Array.from(repeatRow.children).forEach((child) => child !== wrapperVideo && newWrapper.appendChild(child));
           repeatRow.appendChild(newWrapper);
         }
       });
@@ -117,7 +119,7 @@ export default async function decorate(block) {
           const wrapperImage = imageSlot.parentElement.closest('div');
 
           const newWrapper = createTag('div');
-          Array.from(data.children).forEach((child) => child !== wrapperImage && newWrapper.appendChild(child) );
+          Array.from(data.children).forEach((child) => child !== wrapperImage && newWrapper.appendChild(child));
           data.appendChild(newWrapper);
         }
       });
@@ -146,7 +148,12 @@ export default async function decorate(block) {
   const columnList = block.querySelectorAll('.columns > div');
   if (columnList.length > 0) {
     columnList[0].classList.add("first-column-div");
-    columnList[columnList.length - 1].classList.add("last-column-div");
+    if (isBackground) {
+      columnList[columnList.length - 2].classList.add("last-column-div");
+    }
+    else {
+      columnList[columnList.length - 1].classList.add("last-column-div");
+    }
   }
 
   if (!block.classList.contains('link')) {
@@ -225,8 +232,7 @@ export default async function decorate(block) {
   block.querySelectorAll('div > div.second-column').forEach((secondColumn) => {
     const prevElement = secondColumn.querySelector('p.icon-container')?.previousElementSibling;
     // Only wrap in prdouct link container div if element container icon container
-    if (prevElement)
-    {
+    if (prevElement) {
       const productLinkContainer = createTag('div', { class: 'product-link-container' });
       secondColumn.querySelectorAll('p.icon-container').forEach((innerSecond) => {
         productLinkContainer.append(innerSecond);
@@ -247,17 +253,22 @@ export default async function decorate(block) {
     // in devdocs , slot have the lists
     block.querySelectorAll('div.listing').forEach(data => {
       data.closest('div')?.classList.add('button-group-container');
-    
+
       const a = data.querySelector('a');
       if (!a) return;
-    
+
       a.className = '';
       const p = createTag('p', { class: 'button-container' });
       p.append(a);
-    
+
       data.replaceChildren(p);
     });
 
+  }
+
+  if (isBackground) {
+    console.log("block.querySelector('.columns > div > div:last-child')", block.querySelector('.columns > div > div:last-child'))
+    container.style.backgroundColor = block.querySelector('.columns > div > div:last-child').textContent.trim();
   }
 
   const observer = new IntersectionObserver((entries) => {
