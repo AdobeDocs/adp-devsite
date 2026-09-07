3ff6140feb1fab62119794af65c8031dd7d5af47
BaskarMitrah <mit44186@adobe.com>
2026-09-07 12:55:59 +0530

    fix : external header link issue

--- hlx_statics/blocks/header/header.js
+++ hlx_statics/blocks/header/header.js
@@ -1107,13 +1107,6 @@ export default async function decorate(block) {
     if (topNavHtml) {
       navigationLinks.innerHTML += topNavHtml;
 
-      // external links (aio_external query param)
-      navigationLinks.querySelectorAll('a[href*="aio_external"]').forEach((link) => {
-        link.href = link.href.replace(/[?&]aio_external/, '');
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-      });
-
       // Process dropdowns for documentation template navigation
       navigationLinks.querySelectorAll('li > ul').forEach((dropDownList, index) => {
         let dropdownLinkDropdownHTML = '';
@@ -1133,6 +1126,18 @@ export default async function decorate(block) {
           dropdownLinksHTML,
         );
         dropDownList.parentElement.innerHTML = dropdownLinkDropdownHTML;
+      });
+
+      // external links (aio_external query param)
+      navigationLinks.querySelectorAll('a[href*="aio_external"]').forEach((link) => {
+        try {
+          const url = new URL(link.href, window.location.href);
+          link.href = url.href;
+        } catch (e) {
+          link.href = link.href.replace(/[?&]aio_external(=[^&#]*)?/, '');
+        }
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener noreferrer');
       });
 
       header.append(navigationLinks);
